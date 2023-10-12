b04ac71fafe997f7d7c16163b75fc4b38f84059e
Scythe101 <127077495+Scythe101@users.noreply.github.com>
2023-10-12 17:06:11 +0000

    aaaaa

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var number = 0
 var cpc=1
+var upgrOneVal=0
 function addOne(){
     number += cpc
     
     displayNumChange();
-    save();
+    save("num", number);
 }
 
 function getNumber(){
@@ -15,29 +16,37 @@ function getNumber(){
 function save(){
     localStorage.setItem("num", number);
     localStorage.setItem("cpc", cpc);
+    localStorage.setItem("upgrOneVal", upgrOneVal);
 }
 
 function load(){
     number = parseInt(localStorage.getItem("num"));
     cpc = parseInt(localStorage.getItem("cpc"));
+    upgrOneVal = parseInt(localStorage.getItem("upgrOneVal"));
     displayNumChange();
 }
 
 function reset(){
     number = 0;
+    cpc = 1;
+    upgrOneVal = 0;
+    save();
     displayNumChange();
 }
 
 function buyUpgradeOne(){
-    if (number >= 100){
-        number-=100
-        cpc+=1
+    if (number >= 100+50*(upgrOneVal)){
+        number-=100;
+        cpc+=1;
+        upgrOneVal+=1;
     }
     displayNumChange();
-    save();
+    save("cpc", cpc);
+    save("upgrOneVal", upgrOneVal);
 }
 
 function displayNumChange(){
     document.getElementById("number").innerHTML = "$" + number;
     document.getElementById("cpc").innerHTML = "Dollars per Click: " + cpc;
+    document.getElementById("upgradeOne").innerHTML = "Upgrade One: $" + (100+50*(upgrOneVal));
 }
