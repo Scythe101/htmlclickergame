8ed01df005b2fb5a18d3b6f4038c105389ed41a3
Scythe101 <127077495+Scythe101@users.noreply.github.com>
2023-10-11 22:20:19 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,19 @@ function addOne(){
 
 function getNumber(){
     number = localStorage.getItem("number");
+    addOne();
+}
+
+function save(){
+    localStorage.setItem("num", number);
+}
+
+function load(){
+    number = parseInt(localStorage.getItem("num"))-1;
+    addOne();
+}
+
+function reset(){
+    number = -1;
+    addOne();
 }
